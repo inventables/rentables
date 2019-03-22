94c7adddf3637e49dbfae10e1e7a9edd4ec887f5
Kacper Marcisz <kmarcisz@gmail.com>
2019-03-22 12:26:40 -0500

    Add route showing all the vehicles.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ApplicationRecord
   belongs_to :vehicle_model
+  delegate :make, :vehicle_category, :category_name, :daily_price, to: :vehicle_model
+  delegate :name, to: :vehicle_model, prefix: :model
 end
--- app/models/vehicle_model.rb
+++ app/models/vehicle_model.rb
@@ -1,3 +1,5 @@
 class VehicleModel < ApplicationRecord
   belongs_to :vehicle_category
+  delegate :name, to: :vehicle_category, prefix: :category
+  delegate :daily_price, to: :vehicle_category
 end
