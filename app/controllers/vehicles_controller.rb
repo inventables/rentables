09dc70af6b16d625e35aefb05dceda937dae0eaa
Kacper Marcisz <kmarcisz@gmail.com>
2019-04-05 11:36:41 -0500

    Added the option to create new cars in a form

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,30 @@
 class VehiclesController < ApplicationController
+  before_action :load_vehicle_models, only: [:new, :create]
+
   def index
     @vehicles = Vehicle.all
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new vehicle_params
+
+    if @vehicle.save
+      redirect_to vehicles_path
+    else
+      render action: "new"
+    end
+  end
+
+  private
+  def load_vehicle_models
+    @vehicle_models = VehicleModel.all
+  end
+
+  def vehicle_params
+    params.require(:vehicle).permit(:year, :vehicle_model_id, :commissioned_on, :decommissioned_on)
   end
 end
--- app/models/vehicle_model.rb
+++ app/models/vehicle_model.rb
@@ -2,4 +2,8 @@ class VehicleModel < ApplicationRecord
   belongs_to :vehicle_category
   delegate :name, to: :vehicle_category, prefix: :category
   delegate :daily_price, to: :vehicle_category
+
+  def make_and_name
+    "#{make} #{name}"
+  end
 end
