7da18baf763d72ec954313b5a7b04fc77e08c8e4
Kacper Marcisz <kmarcisz@gmail.com>
2019-04-08 11:17:07 -0500

    Test if we can make a reservation when we need to account for vehicles available in stock

---
+++ test/integration/reservation_flow_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class ReservationFlowTest < ActionDispatch::IntegrationTest
+  test "should create a reservation if a car is available to be rented" do
+    FactoryBot.create :vehicle # this also creates the VehicleModel and VehicleCategory
+    vehicle_category = VehicleCategory.first
+
+    # lets create 2 reservations that allow for another reservation in between them
+    FactoryBot.create :reservation, vehicle_category: vehicle_category, start_date: "2019-01-01", end_date: "2019-01-10"
+    FactoryBot.create :reservation, vehicle_category: vehicle_category, start_date: "2019-01-21", end_date: "2019-01-30"
+
+    # we should be able to make a reservation in the empty space between existing reservations
+    post reservations_path, params: { reservation: { start_date: "2019-01-11", end_date: "2019-01-20", vehicle_category_id: vehicle_category.id } }
+    follow_redirect!
+    assert_response :success
+    assert_equal 3, Reservation.count
+  end
+
+  test "cannot create a reservation when no vehicles are available" do
+    FactoryBot.create :vehicle # this also creates the VehicleModel and VehicleCategory
+    vehicle_category = VehicleCategory.first
+
+    # lets create a reservation that lasts the entire month
+    FactoryBot.create :reservation, vehicle_category: vehicle_category, start_date: "2019-01-01", end_date: "2019-01-31"
+
+    # by having only one car we should not be able to make new reservations
+    post reservations_path, params: { reservation: { start_date: "2019-01-11", end_date: "2019-01-20", vehicle_category_id: vehicle_category.id } }
+    follow_redirect!
+    assert_response :success
+    assert_equal 1, Reservation.count
+  end
+end
