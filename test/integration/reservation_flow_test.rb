9f0372a58535633924bef38fc4960826daf8284c
Kacper Marcisz <kmarcisz@gmail.com>
2019-04-09 10:01:12 -0500

    Adding a large reservation test

--- test/integration/reservation_flow_test.rb
+++ test/integration/reservation_flow_test.rb
@@ -29,4 +29,43 @@ class ReservationFlowTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal 1, Reservation.count
   end
+
+  test "creating new reservations with a lot of existing data should complete in reasonable time" do
+    CATEGORY_COUNT = 5
+    MODEL_COUNT = 5
+    VEHICLE_COUNT = 10
+    NUMBER_OF_WEEKS_RENTED = 4
+    EXPECTED_RESERVATION_COUNT = CATEGORY_COUNT * MODEL_COUNT * VEHICLE_COUNT * NUMBER_OF_WEEKS_RENTED
+
+      CATEGORY_COUNT.times { FactoryBot.create :vehicle_category }
+
+    VehicleCategory.all.each do |vehicle_category|
+      MODEL_COUNT.times { FactoryBot.create :vehicle_model, vehicle_category: vehicle_category }
+    end
+
+    VehicleModel.all.each do |vehicle_model|
+      VEHICLE_COUNT.times { FactoryBot.create :vehicle, vehicle_model: vehicle_model }
+    end
+
+    start_date = Date.parse("2019-01-01")
+
+    # each category has MODEL_COUNT models, and each model has VEHICLE_COUNT vehicles available, therefore we can rent MODEL_COUNT * VEHICLE_COUNT cars of each category
+    vehicle_categories = VehicleCategory.all
+
+    # we generate a category spanning from a week each for the first 4 weeks of the 2019 for each vehicle
+    (MODEL_COUNT * VEHICLE_COUNT).times do
+      vehicle_categories.each do |vehicle_category|
+        (1..NUMBER_OF_WEEKS_RENTED).each do |i|
+          reservation_start_date = start_date + (7 * (i - 1).days)
+          FactoryBot.create :reservation, vehicle_category: vehicle_category, start_date: reservation_start_date, end_date: reservation_start_date + 6
+        end
+      end
+    end
+
+    # we should not be able to generate a reservation for January as all the vehicles are booked
+    post reservations_path, params: { reservation: { start_date: "2019-01-11", end_date: "2019-01-20", vehicle_category_id: VehicleCategory.first.id } }
+    follow_redirect!
+    assert_response :success
+    assert_equal EXPECTED_RESERVATION_COUNT, Reservation.count
+  end
 end
