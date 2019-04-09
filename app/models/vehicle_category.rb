f786df70c96a79d9da989a2e0d3e193dfada969b
James Rodovich <jim@inventables.com>
2019-04-09 11:40:54 -0500

    Add has_many relations.

--- app/models/vehicle_category.rb
+++ app/models/vehicle_category.rb
@@ -1,3 +1,4 @@
 class VehicleCategory < ApplicationRecord
   has_many :vehicle_models
+  has_many :vehicles, through: :vehicle_models
 end
--- app/models/vehicle_model.rb
+++ app/models/vehicle_model.rb
@@ -1,5 +1,7 @@
 class VehicleModel < ApplicationRecord
   belongs_to :vehicle_category
+  has_many :vehicles
+
   delegate :name, to: :vehicle_category, prefix: :category
   delegate :daily_price, to: :vehicle_category
 
