9cac730c13f7390f1bd8a58a946bba12c25a16a8
James Rodovich <jim@inventables.com>
2019-04-15 16:50:58 -0500

    Add another test case.

--- test/integration/reservation_flow_test.rb
+++ test/integration/reservation_flow_test.rb
@@ -31,4 +31,19 @@ class ReservationFlowTest < ActionDispatch::IntegrationTest
     # the new reservation should not be saved
     assert_equal 1, Reservation.count
   end
+
+  test "cannot create a reservation when no vehicles are available during the entire time range" do
+    vehicle_category = FactoryBot.create :vehicle_category
+    vehicle_model = FactoryBot.create :vehicle_model, vehicle_category: vehicle_category
+    FactoryBot.create :vehicle, vehicle_model: vehicle_model
+
+    # create a short reservation in the middle of the month
+    FactoryBot.create :reservation, vehicle_category: vehicle_category, start_date: "2019-01-15", end_date: "2019-01-16"
+
+    # by having only one car we should not be able to make new reservations including those days
+    post reservations_path, params: { reservation: { start_date: "2019-01-11", end_date: "2019-01-20", vehicle_category_id: vehicle_category.id } }
+
+    # the new reservation should not be saved
+    assert_equal 1, Reservation.count
+  end
 end
