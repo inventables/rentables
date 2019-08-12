1f1d9d8ac2bb57a4b15e3b3b6a08e565f604c2eb
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2019-08-12 11:38:00 -0500

    implement validation logic to prevent overlapping reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,6 +8,7 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates :vehicle_category, presence: true
   validate :end_date_has_to_be_after_start_date
+  validate :no_conflicting_reservations
 
   private
   def set_total_price
@@ -18,5 +19,47 @@ class Reservation < ApplicationRecord
     if end_date.present? && start_date.present? && end_date <= start_date
       errors.add(:end_date, "can't be before or on start date")
     end
+  end
+
+  def no_conflicting_reservations
+    errors.add(:base, "Reservation cannot overlap with an existing reservation") if conflicting_reservations.present?
+  end
+
+  def conflicting_reservations
+    Reservation.where(overlap_case_1).or(
+      Reservation.where(overlap_case_2).or(
+        Reservation.where(overlap_case_3).or(
+          Reservation.where(overlap_case_4)
+        )
+      )
+    )
+  end
+
+  def overlap_case_1
+    <<~EOS
+      start_date <= '#{start_date}'
+      AND end_date >= '#{start_date}'
+    EOS
+  end
+
+  def overlap_case_2
+    <<~EOS
+      start_date <= '#{end_date}'
+      AND end_date >= '#{end_date}'
+    EOS
+  end
+
+  def overlap_case_3
+    <<~EOS
+      start_date <= '#{start_date}'
+      AND end_date >= '#{end_date}'
+    EOS
+  end
+
+  def overlap_case_4
+    <<~EOS
+      start_date >= '#{start_date}'
+      AND end_date <= '#{end_date}'
+    EOS
   end
 end
