d5057947970e51786a95696e2293398fd105af85
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2019-08-12 13:50:47 -0500

    update conflicting check to account for vehicles available in the fleet

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,23 +8,13 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates :vehicle_category, presence: true
   validate :end_date_has_to_be_after_start_date
-  validate :no_conflicting_reservations
+  validate :vehicle_available
 
 
-  # made this method a public instance
-  # because it could be valuable to pull a list of
-  # conflicting reservations
-  def conflicting_reservations
-    vehicle_category_reservations.where(overlap_case_1).or(
-      vehicle_category_reservations.where(overlap_case_2).or(
-        vehicle_category_reservations.where(overlap_case_3).or(
-          vehicle_category_reservations.where(overlap_case_4)
-        )
-      )
-    )
-  end
+
 
   private
+
   def set_total_price
     update_attribute :total_price, vehicle_category.daily_price * (end_date - start_date).to_i
   end
@@ -35,36 +25,44 @@ class Reservation < ApplicationRecord
     end
   end
 
-  def no_conflicting_reservations
-    errors.add(:base, "Reservation cannot overlap with existing reservations") if conflicting_reservations.present?
+  def vehicle_available
+    errors.add(:base, "#{category_name} not available to reserve #{start_date} to #{end_date}.") if conflicting_reservations.count >= vehicle_category.vehicles.count
   end
 
-  def vehicle_category_reservations
-    Reservation.where(vehicle_category: vehicle_category)
+  def conflicting_reservations
+    category_reservations = self.vehicle_category.reservations
+
+    category_reservations.where(conflict_case_1).or(
+      category_reservations.where(conflict_case_2).or(
+        category_reservations.where(conflict_case_3).or(
+          category_reservations.where(conflict_case_4)
+        )
+      )
+    ).uniq
   end
 
-  def overlap_case_1
+  def conflict_case_1
     <<~EOS
       start_date <= '#{start_date}'
       AND end_date >= '#{start_date}'
     EOS
   end
 
-  def overlap_case_2
+  def conflict_case_2
     <<~EOS
       start_date <= '#{end_date}'
       AND end_date >= '#{end_date}'
     EOS
   end
 
-  def overlap_case_3
+  def conflict_case_3
     <<~EOS
       start_date <= '#{start_date}'
       AND end_date >= '#{end_date}'
     EOS
   end
 
-  def overlap_case_4
+  def conflict_case_4
     <<~EOS
       start_date >= '#{start_date}'
       AND end_date <= '#{end_date}'
